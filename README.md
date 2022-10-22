6f6047a520bb15e4f0da50ac0af57ce60a3c6747
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-21 23:08:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# movie_wiki
+ Projeto com React, consumindo a API do TMDB (filmes), utilizando hooks e React Router, criado com Vite.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>MovieWiki</h2>
+    </div>
+  )
+}
+
+export default App
